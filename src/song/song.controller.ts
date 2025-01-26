e5d2375f6a07715ad5ba89cbfcd6bc7a5c1e34a0
ichiragkumar <chiragroy3030@gmail.com>
2025-01-26 22:23:57 +0530

    added all routes of songs application

--- src/song/song.controller.ts
+++ src/song/song.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { SongService } from './song.service';
 
 @Controller('song')
-export class SongController {}
+export class SongController {
+    constructor(private readonly songService: SongService) {}
+
+
+    @Post('/playlist')
+    craetePlayList(@Body() playlistName: string) {
+        return this.songService.craetePlayList(playlistName);
+    }
+
+    @Get('/playlists')
+    getAllPlaylists() {
+        return this.songService.getAllPlaylists();
+    }
+
+    @Put('/playlists/:id')
+    updatePlayListName(@Param('id') playlistId: string) {
+        return this.songService.updatePlayListName(playlistId);
+    }
+
+    @Delete('/playlists/:id')
+    removeSongFromPlaylist(@Param('id') playlistId: string) {
+        return this.songService.removeSongFromPlaylist(playlistId);
+    }
+
+    @Post('/playlists/:id/song/:songId')
+    addSongToPlaylist(
+        @Param('id') playlistId: string,
+        @Param('songId') songId: string
+    ) {
+        const songaData = { playlistId, songId };
+        return this.songService.addSongToPlaylist(songaData);
+    }
+    
+    @Get('/songs/:id')
+    getSongById(@Body() playlistId: string) {
+        return this.songService.getSongFromPlaylistById(playlistId);
+    }
+
+    @Get("/songs")
+    getAllSongs() {
+        return this.songService.getAllSongs();
+    }
+}
--- src/song/song.service.ts
+++ src/song/song.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongService {}
+export class SongService {
+        craetePlayList(playlistName:string) {
+            return {
+                msg : "Playlist Created",
+                data : playlistName
+            }
+        }
+
+        getAllPlaylists(): string {
+            return "All Playlists"
+        }
+
+        updatePlayListName(playlistId:string):string {
+            return `Playlist with id ${playlistId} updated`;
+        }
+
+        removeSongFromPlaylist(playlistId: string):string {
+            return `song with id ${playlistId} deleted`;
+        }
+
+    
+        addSongToPlaylist(songaData: { playlistId: string; songId: string }): string {
+            return `Song with id ${songaData.songId} added to playlist {playlistId: ${songaData.playlistId}}`;
+        }
+        
+
+        getSongFromPlaylistById(playlistId: string):string {
+            return `Song with id ${playlistId} found`;
+        }
+
+
+        getAllSongs(): any {
+            const allSongs = [
+                {
+                    id: 1,
+                    name: "song1",
+                    description: "song1 description",
+                    date: "2022-01-01",
+                },
+                {
+                    id: 2,
+                    name: "song2",
+                    description: "song2 description",
+                    date: "2022-01-02",
+                },
+                {
+                    id: 3,
+                    name: "song3",
+                    description: "song3 description",
+                    date: "2022-01-03",
+                },
+            ];
+
+            return {                
+                msg : "Songs Found",
+                data : allSongs
+            }
+    
+        }
+
+
+
+
+
+}
