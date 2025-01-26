1a23848579844ef759332e3736740e870d720ddb
ichiragkumar <chiragroy3030@gmail.com>
2025-01-26 21:48:48 +0530

    implemented social module for work management app

--- src/social/social.controller.ts
+++ src/social/social.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { SocialService } from './social.service';
 
 @Controller('social')
-export class SocialController {}
+export class SocialController {
+    constructor(private readonly socialService: SocialService) {}
+
+
+
+
+
+    @Post('/login')
+    login(@Body() data: {
+        username: string,
+        password: string
+    }) {
+        return this.socialService.login(data);
+    }
+
+
+    @Post('/signup')
+    signup(@Body() data: {
+        username: string,
+        password: string,
+        email: string
+    }) {
+        return this.socialService.signup(data);
+    }
+
+
+    @Post('/logout')
+    logout(@Body() data: {
+        username: string,
+        password: string
+    }) {
+        return this.socialService.logout(data);
+    }
+
+    @Get('/getuser/:email')
+    getUser(@Param('email') email: string) {
+        return this.socialService.getUser(email);
+    }
+
+
+    @Get('/getusers')
+    getUsers() {
+        return this.socialService.getUsers();
+    }
+
+    @Put("/forgotpassword")
+    forgotPassword(@Body () data: {
+        username:string,
+        email:string
+    }) {
+        return this.socialService.forgotPassword(data);
+    }
+
+
+}
--- src/social/social.service.ts
+++ src/social/social.service.ts
@@ -1,4 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 
 @Injectable()
-export class SocialService {}
+export class SocialService {
+
+
+        login(data:any) {
+            return {
+                msg : "Login Successful",
+                data : data
+            };
+        }
+        signup(data:any) {
+            return {
+                msg : "Signup Successful",
+                data : data
+            };
+
+        }        
+        logout(data:any) {
+            return {
+                msg : "Logout Successful",
+                data : null
+            }
+        }
+
+
+        getUser(email:string) {
+            return `User with email ${email} found`;
+        }
+
+      
+
+        getUsers() {
+            const allUsers = [
+                {
+                    id: 1,
+                    username: "ichirag",
+                    email: "ichiragkumar.dev@gmail.com",
+                    password: "123456",
+                    socialMedia: {
+                        facebook: "ichiragkumar.dev",
+                        twitter: "ichiragkumar",
+                        instagram: "ichiragkumar",
+                        linkedin: "ichiragkumar",
+                        github: "ichiragkumar",
+                    },
+                },
+                {
+                    id: 2,
+                    username: "ichirag2",
+                    email: "ichiragkumar2@gmail.com",
+                    password: "123456",
+                    socialMedia: {
+                        facebook: "ichiragkumar2",
+                        twitter: "ichiragkumar2",
+                        instagram: "ichiragkumar2",
+                        linkedin: "ichiragkumar2",
+                        github: "ichiragkumar2",
+                    },
+                },  
+                {
+                    id: 3,
+                    username: "ichirag3",
+                    email: "ichiragkumar3@gmail.com",
+                    password: "123456",
+                    socialMedia: {
+                        facebook: "ichiragkumar3",
+                        twitter: "ichiragkumar3",
+                        instagram: "ichiragkumar3",
+                        linkedin: "ichiragkumar3",
+                        github: "ichiragkumar3",
+                    },
+                },
+            ]
+            return {
+                msg : "Users Found",
+                data : allUsers
+            }
+        }
+
+
+        forgotPassword(data:{
+            username:string,
+            email:string
+        }) {
+            return "Forgot Password Successful";
+        }       
+
+
+}
